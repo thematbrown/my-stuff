ace7e0c6f7b052985f43dc441262c36ca66d63df
Matthew Brown <mjbme10@yahoo.com>
2022-04-08 12:40:39 -0500

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+
+# Install misc programs
+sudo apt install tilix zsh
+
+# Install Docker
+sudo apt install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update 
+sudo apt install docker-ce docker-ce-cli containerd.io
+
+# Install Terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+
+# Install Pyenv
+curl https://pyenv.run | bash
